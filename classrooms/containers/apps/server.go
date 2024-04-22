5e38b75bf3096ecf135c7bb8cc852519be815b81
André Tonelli <andreveigatonelli@gmail.com>
2024-04-22 13:47:53 +0100

    class: k8s health check, healthz endpoint

--- classrooms/containers/apps/server.go
+++ classrooms/containers/apps/server.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secrets", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8000", nil)
 }
@@ -22,11 +26,30 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
 
-	data, err := ioutil.ReadFile("myfamily/family.txt")
+	data, err := os.ReadFile("myfamily/family.txt")
 
 	if err != nil {
 		log.Fatalf("Error reading file: ", err)
 	}
 
 	fmt.Fprintf(w, "The folks %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "user %s. password %s", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
