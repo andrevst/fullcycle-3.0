fd7e78f0be80c7d6cbdcc263707d2881d9dbd489
André Tonelli <andreveigatonelli@gmail.com>
2024-05-12 17:38:01 +0100

    class: terraform, feat: update network add tags to subnets

--- classrooms/iac/terraform/vpc.tf
+++ classrooms/iac/terraform/vpc.tf
@@ -1,10 +1,6 @@
 
 data "aws_availability_zones" "available_zones" {}
 
-output "available_zones" {
-    value = "${data.aws_availability_zones.available_zones.names}"
-}
-
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -17,10 +13,18 @@ resource "aws_subnet" "eks_sub_1" {
     vpc_id = aws_vpc.eks_vpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
+    tags = {
+        Name = "eks-subnet-1"
+        project = "fullcycle-3.0"
+  }
 }
 
 resource "aws_subnet" "eks_sub_2" {
     vpc_id = aws_vpc.eks_vpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1b"
+    tags = {
+        Name = "eks-subnet-2"
+        project = "fullcycle-3.0"
+  }
 }
