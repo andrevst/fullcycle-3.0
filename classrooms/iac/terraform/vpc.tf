2a74318085c82e292337424052b172ca06c40744
André Tonelli <andreveigatonelli@gmail.com>
2024-05-13 08:02:30 +0100

    class: terraform, feat: add route table and internet gateway, update it in docs

--- classrooms/iac/terraform/vpc.tf
+++ classrooms/iac/terraform/vpc.tf
@@ -20,3 +20,23 @@ resource "aws_subnet" "eks_subnets" {
     project = var.project
   }
 }
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name    = "eks-igw"
+    project = var.project
+  }
+}
+
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+  tags = {
+    Name    = "eks-route-table"
+    project = var.project
+  }
+}
