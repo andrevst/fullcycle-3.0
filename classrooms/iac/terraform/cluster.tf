08392d9d3be63a6116b56a2ff2859152947c2761
André Tonelli <andreveigatonelli@gmail.com>
2024-05-14 16:19:40 +0100

    class: terraform, feat: add nodegroup

--- classrooms/iac/terraform/cluster.tf
+++ classrooms/iac/terraform/cluster.tf
@@ -45,6 +45,11 @@ resource "aws_cloudwatch_log_group" "cluster_log_group" {
   name              = "/aws/eks/${var.cluster_name}"
   retention_in_days = var.retention_days
 
+  tags = {
+    name    = "${var.cluster_name}-logs"
+    project = var.project
+  }
+
 }
 
 resource "aws_eks_cluster" "cluster" {
@@ -60,6 +65,73 @@ resource "aws_eks_cluster" "cluster" {
   vpc_config {
     subnet_ids         = aws_subnet.eks_subnets[*].id
     security_group_ids = [aws_security_group.eks_sg.id]
+  }
+
+  tags = {
+    name    = var.cluster_name
+    project = var.project
+  }
+
+}
+
+resource "aws_iam_role" "node_role" {
+  name               = "${var.cluster_name}-node-role"
+  assume_role_policy = <<POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+            "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+        ]
+    }
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_role.name
+
+}
+
+resource "aws_eks_node_group" "node_group" {
+
+  depends_on = [
+    aws_eks_cluster.cluster,
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly
+  ]
+
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = aws_subnet.eks_subnets[*].id
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+  instance_types = ["${var.instance_type}"]
+  # capacity_type  = var.capacity_type
+
+  tags = {
+    name    = "${var.cluster_name}-ng"
+    project = var.project
   }
 
 }
