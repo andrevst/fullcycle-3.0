e80fc570b9771cff5615fbae01d4cc4990f871c9
André Tonelli <andreveigatonelli@gmail.com>
2024-06-12 05:56:00 +0100

    project: codeflix-client, chore: fix header refactor

--- projects/codeflix-client/src/app/components/Header.tsx
+++ projects/codeflix-client/src/app/components/Header.tsx
@@ -1,5 +1,68 @@
 'use client';
 
+import React, { useState, useEffect } from 'react';
 
-;
 
+
+export default function Header() {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const Logo = () => (
+        <img
+            src='./Netflix_logo.svg'
+            alt='netflix'
+            width={120}
+            height={120} />
+    );
+
+    const NavLinks = () => (
+        <nav>
+            <ul className='hidden md:flex md:space-x-4'>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>Latest</li>
+            </ul>
+        </nav>
+    );
+
+    const UserProfile = () => (
+        <div className='flex items-center space-x-4'>
+            <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+            <img
+                src='./profile.png'
+                alt='user'
+                width={40}
+                height={40} />
+        </div>
+    );
+
+    return (
+        <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}>
+            <div className='flex items-center space-x-2 md:space-x-8'>
+
+                <Logo />
+                <NavLinks />
+
+            </div>
+            <UserProfile />
+
+        </header>
+    );
+}
--- projects/codeflix-client/src/app/page.tsx
+++ projects/codeflix-client/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import Header from './components/Header.1';
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className='relative h-full lg:h-[140vh]'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
